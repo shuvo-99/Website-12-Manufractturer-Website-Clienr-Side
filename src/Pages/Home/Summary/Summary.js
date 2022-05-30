4fcef67bf1582dbe81f9b4b46cf801dd4fde01a3
shuvo-99 <anontoshuvo99@gmail.com>
2022-05-30 12:19:30 +0600

    summary

--- src/Pages/Home/Summary/Summary.js
+++ src/Pages/Home/Summary/Summary.js
@@ -3,7 +3,9 @@ import React from "react";
 const Summary = () => {
   return (
     <div>
-      <h2>This is About</h2>
+      <h2 className="text-center text-3xl font-bold uppercase">
+        Bussiness Summary
+      </h2>
     </div>
   );
 };
